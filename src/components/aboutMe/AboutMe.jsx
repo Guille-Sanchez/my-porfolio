d69921eb2a844e91688ac94c04a75341698a2528
Guillermo Sanchez <guillesafran@gmail.com>
2023-03-20 10:34:53 -0300

    Add e-commerce and product-card project to portfolio

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -6,14 +6,14 @@ function AboutMe () {
       <div className='about-me' id='about-me'>
         <h1>Front-End Developer</h1>
         <p>
-          Hi! I am Guillermo. A little bit about me, it's that I am a self-taught front-end developer.
+          Hi! I am Guillermo. A little bit about myselft is that I am a self-taught front-end developer.
           Not surprisingly, the first programming language I picked up was Python due to its simplicity and high functionality.
         </p>
         <p>After some time, I decided to learn a more opinionated language, Ruby with its framework on Rails (RoR).
           Making this change provided me with good programming practices that I still use to this day.
         </p>
         <p>
-          The knowledge acquired in both, Python and Ruby, was vital when learning JavaScript, which with its library React, has won over my heart.
+          The knowledge acquired in both, Python and Ruby, was vital for me when learning JavaScript/TypeScript, which with its library React, has won over my heart.
         </p>
       </div>
     </section>
--- src/components/projects-grid/ProjectsGrid.jsx
+++ src/components/projects-grid/ProjectsGrid.jsx
@@ -6,43 +6,59 @@ function ProjectsGrid () {
   // screenshots iphone 12/13 mini
   const myProjects = [
     {
-      body: 'This project features a controlled form for a greater user experience as well as some sorting mechanisms.',
+      body: 'An easy to use e-commerce website. It connects to an API and allows user to filter the products by price and category.',
       id: 1,
+      image: '../../screenshots/e-commerce-with-api.png',
+      title: 'E-commerce',
+      urlLive: 'https://ecommerce-chichodev.netlify.app/',
+      urlrepo: 'https://github.com/Guille-Sanchez/e-commerce-with-API'
+    },
+    {
+      body: 'This e-commerce product card website is all about design. This projects shows a good eye for details and user experience.',
+      id: 2,
+      image: '../../screenshots/e-commerce.png',
+      title: 'Product card',
+      urlLive: 'https://e-commerce-chichodev.netlify.app/',
+      urlrepo: 'https://github.com/Guille-Sanchez/e-commerce'
+    },
+    {
+      body: 'This project features a controlled form for a greater user experience as well as some sorting mechanisms.',
+      id: 3,
       image: '../../screenshots/buscador-de-peliculas.png',
       title: 'Movie Search Website',
       urlLive: 'https://buscador-de-peliculas-chicho.netlify.app/',
       urlrepo: 'https://github.com/Guille-Sanchez/buscador-de-peliculas'
     }, {
       body: 'Insert a url and shorten it! This website saves your shortened-link on local-storage so you never lose it.',
-      id: 2,
+      id: 4,
       image: '../../screenshots/url-shortener.png',
       title: 'Url Shortener',
       urlLive: 'https://url-shortener-vite-app.netlify.app/',
       urlrepo: 'https://github.com/Guille-Sanchez/url-shortner'
     }, {
       body: 'Bored? This website gives you a random piece of art and tries to guess the artist nationality.',
-      id: 3,
+      id: 5,
       image: '../../screenshots/get-art-piece.png',
       title: 'Get Art Piece',
       urlLive: 'https://art-from-the-art-institute-of-chicago.netlify.app/',
       urlrepo: 'https://github.com/Guille-Sanchez/DisplayArt'
     }, {
       body: 'Manage Company\'s homepage. This project presents small details which put together bring a good user experience.',
-      id: 4,
+      id: 6,
       image: '../../screenshots/manage.png',
       title: 'Get Art Piece',
       urlLive: 'https://glittering-lamington-b0ab80.netlify.app/',
       urlrepo: 'https://github.com/Guille-Sanchez/manage-landing-page'
     }, {
       body: 'Ever wonder what the current transaction for a given currency is? Well, now you can quickly check it on this website.',
-      id: 5,
+      id: 7,
       image: '../../screenshots/bank-exchange.png',
       title: 'Currency Exhange',
       urlLive: 'https://bank-exchange-chicho.netlify.app/',
       urlrepo: 'https://github.com/Guille-Sanchez/bank_exchange'
     }, {
       body: 'Do you have some spare time to kill with a friend? Why don\'t you use it to play the classic game of tic-tac-toe?',
-      id: 6,
+      id: 8,
       image: '../../screenshots/tic-tac-toe.png',
       title: 'Tic-Tac-Toe',
       urlLive: 'https://tic-tac-toe-chicho.netlify.app/',
